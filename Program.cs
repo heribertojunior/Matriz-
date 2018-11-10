3a2f4d231060ac0a017ef90610b81364c8649b08
heribertojunior <44383351+heribertojunior@users.noreply.github.com>
2018-11-09 20:14:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdnome=2;
+
+
+            Console.WriteLine("Digite quantos nomes quer:");
+           qtdnome = Int32.Parse(Console.ReadLine());
+
+            String[,] nome = new string[qtdnome, 2];
+
+            for (int i = 0; i < qtdnome; i++)
+            {
+                for (int j = 0; j < qtdnome; j++)
+                {
+                   
+                    if (j == 1)
+                    {
+                        Console.WriteLine("Dgite o sobrenome da " + (i + 1) + " pessoa");
+                        nome[i, j] = Console.ReadLine();
+                    }else{
+                        Console.WriteLine("Dgite o nome da " + (i + 1) + " pessoa");
+                        nome[i, j] = Console.ReadLine();
+                    }
+                }
+            }
+            for (int i = 0; i < qtdnome; i++)
+            {
+                for (int j = 0; j < qtdnome; j++)
+                {
+                    Console.Write(" " + nome[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
